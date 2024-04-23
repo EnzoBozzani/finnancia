67d13a496d9d4c12bea46fa523d0025c6a310ebd
Enzo Bozzani <bozzanienzo@gmail.com>
2024-04-23 16:57:50 -0300

    feat: addition of features to landing page FAQ section

--- app/(landingPage)/_components/FAQSection.tsx
+++ app/(landingPage)/_components/FAQSection.tsx
@@ -7,11 +7,20 @@ export const FAQSection = () => {
 				<h1 className='text-2xl sm:text-4xl font-semibold text-center'>FAQ</h1>
 				<FAQAccordion>
 					<FAQAccordion.Item value='item-1'>
-						<FAQAccordion.Trigger>Como posso adicionar uma nova finança mensal?</FAQAccordion.Trigger>
+						<FAQAccordion.Trigger>
+							Preciso adicionar meus dados bancários para registrar finanças?
+						</FAQAccordion.Trigger>
 						<FAQAccordion.Content>
-							No painel principal, clique em "Adicionar finança" e preencha os detalhes. Feito isso, a
-							finança será adicionada em cada nova planilha que for criada. Também é possível alterar
-							essas finanças individualmente quando forem inseridas nas planilhas.
+							Não, não é necessário. O Finnancia preza pela segurança, privacidade e flexibilidade.
+							Portanto, não é preciso cadastrar nenhum perfil de instituição financeira para gerenciar
+							suas finanças, sendo possível fazer isso de maneira flexível, simples e prática.
+						</FAQAccordion.Content>
+					</FAQAccordion.Item>
+					<FAQAccordion.Item value='item-7'>
+						<FAQAccordion.Trigger>O Finnancia é pago?</FAQAccordion.Trigger>
+						<FAQAccordion.Content>
+							É possível utilizar o Finnancia sem pagar nada por meio de nosso plano gratuito (o qual não
+							exige o cadastro de nenhum método de pagamento), basta fazer login e aproveitar!
 						</FAQAccordion.Content>
 					</FAQAccordion.Item>
 					<FAQAccordion.Item value='item-2'>
@@ -20,11 +29,11 @@ export const FAQSection = () => {
 						</FAQAccordion.Trigger>
 						<FAQAccordion.Content>
 							Sim, você pode alterar de maneira extremamente simples clicando no ícone de ações e
-							selecionando as opções apresentadas. É possível editar e remover também finanças mensais.
+							selecionando as opções apresentadas. É possível também duplicar uma finança.
 						</FAQAccordion.Content>
 					</FAQAccordion.Item>
 					<FAQAccordion.Item value='item-3'>
-						<FAQAccordion.Trigger>Como registro finanças individuais?</FAQAccordion.Trigger>
+						<FAQAccordion.Trigger>Como registro finanças?</FAQAccordion.Trigger>
 						<FAQAccordion.Content>
 							Para registrar finanças individuais é extremamente simples! Quando estiver em alguma tela de
 							planilha, basta clicar no botão verde no canto inferior direito da tela!
@@ -37,6 +46,23 @@ export const FAQSection = () => {
 						<FAQAccordion.Content>
 							Sim, na página do painel você pode ter uma noção total das suas finanças, mostrando dados
 							como gasto, ganho e saldo médio por mês, saldo total, além de análises de seus dados!
+						</FAQAccordion.Content>
+					</FAQAccordion.Item>
+					<FAQAccordion.Item value='item-5'>
+						<FAQAccordion.Trigger>Como conversar com a FinnancIA?</FAQAccordion.Trigger>
+						<FAQAccordion.Content>
+							Para bater um papo sobre controle financeiro com a IA do Finnancia, basta clicar no ícone
+							"FinnancIA" na barra lateral, a qual irá te redirecionar para o chat!
+						</FAQAccordion.Content>
+					</FAQAccordion.Item>
+					<FAQAccordion.Item value='item-6'>
+						<FAQAccordion.Trigger>
+							Como gerar um relatório personalizado, contendo todas minhas finanças do mês e uma análise
+							de IA em cima dessas finanças?
+						</FAQAccordion.Trigger>
+						<FAQAccordion.Content>
+							Na página da FinnancIA, basta descer até o final da página, selecionar a planilha que deseja
+							e esperar a IA gerar o relatório!
 						</FAQAccordion.Content>
 					</FAQAccordion.Item>
 				</FAQAccordion>
