5a9763930a4e0eb9019cf59360f00d52ddd202aa
Enzo Bozzani <bozzanienzo@gmail.com>
2024-06-12 19:37:18 -0300

    feat: create route to get sheet finances including categories

---
+++ app/api/finances/categories/[sheetId]/route.ts
@@ -0,0 +1,36 @@
+import { currentUser } from '@/lib/auth';
+import { db } from '@/lib/db';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(req: NextRequest, { params }: { params: { sheetId: string } }) {
+	const { sheetId } = params;
+
+	const user = await currentUser();
+
+	if (!user) {
+		return NextResponse.json(
+			{
+				error: 'Não autorizado',
+			},
+			{ status: 401 }
+		);
+	}
+
+	try {
+		const finances = await db.finance.findMany({
+			where: { sheetId, sheet: { userId: user.id } },
+			include: {
+				category: true,
+			},
+		});
+
+		return NextResponse.json({ finances }, { status: 200 });
+	} catch (error) {
+		return NextResponse.json(
+			{
+				error: 'Algo deu errado',
+			},
+			{ status: 500 }
+		);
+	}
+}
--- services/financesService.ts
+++ services/financesService.ts
@@ -70,4 +70,16 @@ export const financesService = {
 
 		return res.json();
 	},
+
+	async getFinancesWithCategories(sheetId: string) {
+		const res = await fetch(`${process.env.NEXT_PUBLIC_APP_URL}/api/finances/categories/${sheetId}`, {
+			method: 'GET',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json',
+			},
+		});
+
+		return res.json();
+	},
 };
